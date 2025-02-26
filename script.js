a336bbce14c3431349a64e61e66dc76babbe7045
WarisRaja <warismehsi0463@gmail.com>
2025-02-26 16:28:22 +0530

    till 5 questions done

---
+++ script.js
@@ -0,0 +1,7 @@
+
+//there are five ways to print any message on console.
+// console.log("this is waris")
+// console.warn("this is warning")
+// console.info("this is info")
+// console.error("this is error")
+// console.table({Name:'waris',age :21})
